8ad59cc46d9ec8469cf7b025a0bd4c394f246b1f
kushal-10 <kushalkoshti11@gmail.com>
2023-07-31 12:23:21 +0200

    update frontend

--- app.py
+++ app.py
@@ -4,24 +4,50 @@ import gradio as gr
 import time
 from hay.pipeline import rs_pipeline
 
-with gr.Blocks() as chat:
-    chatbot = gr.Chatbot()
-    msg = gr.Textbox()
-    clear = gr.ClearButton([msg, chatbot])
+title = """<h1 align="center">Chat Literature</h1>"""
+description = """<br><br><h3 align="center">This is a literature chat model, which can currently answer questions to regulation topic in Supply chain mangement.</h3>"""
 
-    def user(user_message, history):
-        return "", history + [[user_message, None]]
+def user(user_message, history):
+    return "", history + [[user_message, None]]
 
-    def respond(message, chat_history):
-        question = str(message)
-        answer = rs_pipeline(question)
-        bot_message = answer
-        chat_history.append((message, bot_message))
-        time.sleep(2)
-        return " ", chat_history
-    
+def respond(message, chat_history):
+    question = str(message)
+    answer = rs_pipeline(question)
+    bot_message = answer
+    chat_history.append((message, bot_message))
+    time.sleep(2)
+    return " ", chat_history
+
+with gr.Blocks(theme=gr.themes.Soft(primary_hue="emerald", neutral_hue="slate")) as chat:
+    gr.HTML(title)
+    chatbot = gr.Chatbot().style(height=750)
+    msg = gr.Textbox(label="Send a message", placeholder="Send a message",
+                             show_label=False).style(container=False)
+    # with gr.Row():
+    #     with gr.Column():
+    #         msg = gr.Textbox(label="Send a message", placeholder="Send a message",
+    #                          show_label=False).style(container=False)
+    #     with gr.Column():
+    #         with gr.Row():
+                # submit = gr.Button("Submit")
+                # stop = gr.Button("Stop")
+                # clear = gr.Button("Clear")
+
+
     msg.submit(respond, [msg, chatbot], [msg, chatbot])
 
+
+    gr.Examples([
+        ["How to reduce carbon emissions?"],
+        ["What are the main topics in these papers?"],
+        ["What are the major math models in PI."]
+    ], inputs=msg, label= "Click on any example to copy in the chatbox"
+      )
+    gr.HTML(description)
+
 def application():
-    chat.launch()
+    return None
+
+chat.queue()
+chat.launch()
 
